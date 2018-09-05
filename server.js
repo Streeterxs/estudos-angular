a1b08f6889a1accb204eedefad626618d77552ef
Afonso Araújo <afonsomg3@gmail.com>
2018-09-05 04:32:35 -0300

    deploy no heroku final 3

--- server.js
+++ server.js
@@ -1,17 +1,17 @@
+const express= require('express'),
+path = require('path');
 
-//Install express server
-const express = require('express');
-const path = require('path');
+const app =express();
 
-const app = express();
 
-// Serve only the static files form the dist directory
 app.use(express.static('./dist/app-deploy-test'));
 
-app.get('/*', function(req,res) {
-    
+app.get('/*', (req,res)=>{
+
 res.sendFile(path.join(__dirname,'/dist/app-deploy-test/index.html'));
+
 });
 
-// Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 8080, ()=>{
+console.log('Server started');
+})
