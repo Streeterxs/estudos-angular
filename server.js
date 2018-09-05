6295b5d768e9383b66664e310a435e6f0bae993b
Afonso Araújo <afonsomg3@gmail.com>
2018-09-05 04:14:31 -0300

    deploy no heroku final

--- server.js
+++ server.js
@@ -6,11 +6,11 @@ const path = require('path');
 const app = express();
 
 // Serve only the static files form the dist directory
-app.use(express.static('./dist/estudos-angular'));
+app.use(express.static('./dist/app-deploy-test'));
 
 app.get('/*', function(req,res) {
     
-res.sendFile(path.join(__dirname,'/dist/estudos-angular/index.html'));
+res.sendFile(path.join(__dirname,'/dist/app-deploy-test/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
